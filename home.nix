bfbf5d260058c43d16b6d9c9f99700a50ef62682
George Sumpster <hello@grg.smpstr.email>
2023-10-11 15:19:38 -0700

    add extensions, spotify, docker, and chrome

--- home.nix
+++ home.nix
@@ -45,12 +45,20 @@
         enable = true;
         mutableExtensionsDir = false;
         extensions = with pkgs.vscode-extensions; [
+          redhat.vscode-yaml
           jnoortheen.nix-ide
           ms-kubernetes-tools.vscode-kubernetes-tools
           dbaeumer.vscode-eslint
           esbenp.prettier-vscode
           eamodio.gitlens
           github.vscode-pull-request-github
+        ]++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "binary-plist";
+            publisher = "dnicolson";
+            version = "0.11.3";
+            sha256 = "sha256-/Wwoy1EOfTx2QimblmnnnY2xWynofua/v1mR/9oZMAw=";
+          }
         ];
       };
     };
