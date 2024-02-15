f2e0b939b096ed1b1c3022dc425a5695af96a125
George Sumpster <hello@grg.smpstr.email>
2024-02-14 16:50:36 -0800

    add codespaces

--- home.nix
+++ home.nix
@@ -11,6 +11,7 @@
     htop
     kubectl
     kubectx
+    kubernetes-helm
     awscli
     cachix
     zoom-us
@@ -56,8 +57,6 @@
           serayuzgur.crates
           graphql.vscode-graphql-syntax
           graphql.vscode-graphql
-          orta.vscode-jest
-          firsttris.vscode-jest-runner
         ]++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
           {
             name = "binary-plist";
@@ -70,6 +69,24 @@
             publisher = "MS-vsliveshare";
             version = "1.0.5883";
             sha256 = "sha256-BNxLINcbat2F4PHCrKHKIuMpXW1q9aP2SDb0oIv48v0=";
+          }
+          {
+            name = "codespaces";
+            publisher = "GitHub";
+            version = "1.16.10";
+            sha256 = "sha256-xwchSdDVX6F6Nw23mQ/LRc7uXPtm3FA1ZWlm9PXWzuw=";
+          }
+          {
+            name = "vscode-jest";
+            publisher = "orta";
+            version = "5.2.3";
+            sha256 = "sha256-cPHwBO7dI44BZJwTPtLR7bfdBcLjaEcyLVvl2Qq+BgE=";
+          }
+          {
+            name = "vscode-jest-runner";
+            publisher = "firsttris";
+            version = "0.4.72";
+            sha256 = "sha256-1nUpOXdteWsyFYJ2uATCcr1SUbeusmbpa09Bkw9/TZM=";
           }
         ];
       };
